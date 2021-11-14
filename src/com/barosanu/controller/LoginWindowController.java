43a5b8f00376d12b1ed4f55225b447f634558222
ajaybasra <ajay_b@hotmail.co.nz>
2021-11-14 23:01:33 +1300

    first commit, did first 2 sections

---
+++ src/com/barosanu/controller/LoginWindowController.java
@@ -0,0 +1,29 @@
+package com.barosanu.controller;
+
+import com.barosanu.EmailManager;
+import com.barosanu.view.ViewFactory;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController extends BaseController {
+
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private Button errorLabel;
+
+    @FXML
+    private PasswordField passwordField;
+
+    public LoginWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
+        super(emailManager, viewFactory, fxmlName);
+    }
+
+    @FXML
+    void loginButtonAction() {
+        System.out.println("click!");
+    }
+}

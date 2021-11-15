44bb9cea374ba7056ed1be43f3036134455974a9
ajaybasra <ajay_b@hotmail.co.nz>
2021-11-15 14:31:04 +1300

    login window closes when you click login

--- src/com/barosanu/view/ViewFactory.java
+++ src/com/barosanu/view/ViewFactory.java
@@ -3,6 +3,7 @@ package com.barosanu.view;
 import com.barosanu.EmailManager;
 import com.barosanu.controller.BaseController;
 import com.barosanu.controller.LoginWindowController;
+import com.barosanu.controller.MainWindowController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -23,8 +24,18 @@ public class ViewFactory {
         System.out.println("show login window called");
 
         BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
-        fxmlLoader.setController(controller);
+        initializeStage(controller);
+    }
+    public void showMainWindow(){
+        System.out.println("main window called");
+
+        BaseController controller = new MainWindowController(emailManager, this, "MainWindow.fxml");
+        initializeStage(controller);
+    }
+
+    private void initializeStage(BaseController baseController) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(baseController.getFxmlName()));
+        fxmlLoader.setController(baseController);
         Parent parent;
         try {
             parent = fxmlLoader.load();
@@ -37,4 +48,9 @@ public class ViewFactory {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void closeStage(Stage stageToClose){
+        stageToClose.close(); //closes stage
+    }
+
 }

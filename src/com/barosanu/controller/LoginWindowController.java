a9a38a17a1155c2786c916cd84340dde6921dad8
ajaybasra <ajay_b@hotmail.co.nz>
2021-11-16 16:16:56 +1300

    intro to javafx services

--- src/com/barosanu/controller/LoginWindowController.java
+++ src/com/barosanu/controller/LoginWindowController.java
@@ -5,7 +5,7 @@ import com.barosanu.controller.services.LoginService;
 import com.barosanu.model.EmailAccount;
 import com.barosanu.view.ViewFactory;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -16,7 +16,7 @@ public class LoginWindowController extends BaseController {
     private TextField emailAddressField;
 
     @FXML
-    private Button errorLabel;
+    private Label errorLabel;
 
     @FXML
     private PasswordField passwordField;
@@ -27,21 +27,26 @@ public class LoginWindowController extends BaseController {
 
     @FXML
     void loginButtonAction() {
+        System.out.println("loginButtonAction!");
         if(fieldsAreValid()){
             EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(), passwordField.getText());
             LoginService loginService = new LoginService(emailAccount, emailManager);
-            EmailLoginResult emailLoginResult = loginService.login();
+            loginService.start();
+            loginService.setOnSucceeded(event -> {
+                EmailLoginResult emailLoginResult = loginService.getValue();
+                switch (emailLoginResult) {
+                    case SUCCESS:
+                        System.out.println("login successful!" + emailAccount);
+                        viewFactory.showMainWindow();
+                        Stage stage = (Stage) errorLabel.getScene().getWindow();
+                        viewFactory.closeStage(stage);
+                }
 
-            switch (emailLoginResult) {
-                case SUCCESS:
-                    System.out.println("login successful!" + emailAccount);
-                    return;
-            }
+            });
+
         }
-        System.out.println("loginButtonAction!");
-        viewFactory.showMainWindow();
-        Stage stage = (Stage) errorLabel.getScene().getWindow();
-        viewFactory.closeStage(stage);
+
+
     }
 
     private boolean fieldsAreValid() {

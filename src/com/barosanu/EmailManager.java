25c0d2dc7e05e235d83db33590d938b412fcde80
ajaybasra <ajay_b@hotmail.co.nz>
2021-11-18 16:52:19 +1300

    clicking msg makes it read

--- src/com/barosanu/EmailManager.java
+++ src/com/barosanu/EmailManager.java
@@ -3,14 +3,36 @@ package com.barosanu;
 import com.barosanu.controller.services.FetchFoldersService;
 import com.barosanu.controller.services.UpdateFoldersService;
 import com.barosanu.model.EmailAccount;
+import com.barosanu.model.EmailMessage;
 import com.barosanu.model.EmailTreeItem;
 import javafx.scene.control.TreeItem;
 
+import javax.mail.Flags;
 import javax.mail.Folder;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmailManager {
+
+    private EmailMessage selectedMessage;
+    private EmailTreeItem<String> selectedFolder;
+
+    public EmailMessage getSelectedMessage() {
+        return selectedMessage;
+    }
+
+    public void setSelectedMessage(EmailMessage selectedMessage) {
+        this.selectedMessage = selectedMessage;
+    }
+
+    public EmailTreeItem<String> getSelectedFolder() {
+        return selectedFolder;
+    }
+
+    public void setSelectedFolder(EmailTreeItem<String> selectedFolder) {
+        this.selectedFolder = selectedFolder;
+    }
+
 
     private UpdateFoldersService updateFoldersService;
     //Folder handling
@@ -35,5 +57,15 @@ public class EmailManager {
         FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
          fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
+    }
+
+    public void setRead() {
+        try {
+            selectedMessage.setRead(true);
+            selectedMessage.getMessage().setFlag(Flags.Flag.SEEN, true);
+            selectedFolder.decrementMessagesCount();
+        } catch (Exception e) {
+
+        }
     }
 }

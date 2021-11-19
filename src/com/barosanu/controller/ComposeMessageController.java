30744acf5572fdd94aa757237305793c171de33a
ajaybasra <ajay_b@hotmail.co.nz>
2021-11-19 13:06:17 +1300

    did controller

--- src/com/barosanu/controller/ComposeMessageController.java
+++ src/com/barosanu/controller/ComposeMessageController.java
@@ -1,13 +1,21 @@
 package com.barosanu.controller;
 
 import com.barosanu.EmailManager;
+import com.barosanu.controller.services.EmailSenderService;
+import com.barosanu.model.EmailAccount;
 import com.barosanu.view.ViewFactory;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.web.HTMLEditor;
+import javafx.stage.Stage;
 
-public class ComposeMessageController extends BaseController{
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ComposeMessageController extends BaseController implements Initializable {
 
     @FXML
     private Label errorLabel;
@@ -23,11 +31,40 @@ public class ComposeMessageController extends BaseController{
 
     @FXML
     void sendButtonAction() {
-        System.out.println(htmlEditor.getHtmlText());
-        System.out.println("send button!");
+        EmailSenderService emailSenderService = new EmailSenderService(
+                emailAccountChoice.getValue(),
+                subjectTextField.getText(),
+                recipientTextField.getText(),
+                htmlEditor.getHtmlText()
+        );
+        emailSenderService.start();
+        emailSenderService.setOnSucceeded(e->{
+            EmailSendingResult emailSendingResult = emailSenderService.getValue();
+            switch (emailSendingResult) {
+                case SUCCESS:
+                    Stage stage = (Stage) recipientTextField.getScene().getWindow();
+                    viewFactory.closeStage(stage);
+                    break;
+                case FAILED_BY_PROVIDER:
+                    errorLabel.setText("Provider error!");
+                    break;
+                case FAILED_BY_UNEXPECTED_ERROR:
+                    errorLabel.setText("Unexpected error!");
+                    break;
+            }
+        });
     }
+
+    @FXML
+    private ChoiceBox<EmailAccount> emailAccountChoice;
 
     public ComposeMessageController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
         super(emailManager, viewFactory, fxmlName);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        emailAccountChoice.setItems(emailManager.getEmailAccounts());
+        emailAccountChoice.setValue(emailManager.getEmailAccounts().get(0));
     }
 }
